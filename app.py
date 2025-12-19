a0e9f575f9728c7e3cdd773b74d4ce8dac346014
Deadspree <minhc613@gmail.com>
2025-12-19 16:39:33 +0900

    [feat] update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def tracking_streamlit(input_video_name: str, model_weight: str = "best.pt"):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    fourcc = cv2.VideoWriter_fourcc(*"H264")
+    fourcc = cv2.VideoWriter_fourcc(*"X264")
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
     # 3️⃣ Initialize SORT tracker
